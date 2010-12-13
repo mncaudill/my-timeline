2a4d668ad45c0ce9bfcb278a00280d4972fef641
Nolan Caudill <nolan@nolancaudill.com>
2010-12-12 19:40:35 -0800

    Added config

---
+++ include/config-sample.php
@@ -0,0 +1,17 @@
+<?php
+
+    $services_auth = array(
+        'cloudmade' => array(
+            'api_key' => '',
+        ),
+        'flickr' => array(
+            'key' => '',
+            'secret' => '',
+        ),
+    );
+
+    $services = array(
+        'flickr' => 1,
+        'twitter' => 2,
+    );
+
