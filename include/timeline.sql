92665cc1e9d65fd3a558a0444e328d425aca8084
Nolan Caudill <nolan@nolancaudill.com>
2010-12-15 21:59:04 -0800

    Added SQL

---
+++ include/timeline.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `users` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `urlname` varchar(100) DEFAULT NULL,
+  `flickr_nsid` varchar(255) DEFAULT NULL,
+  `twitter_screenname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
+CREATE TABLE `geopoints` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `source` int(11) NOT NULL,
+  `source_id` varchar(50) NOT NULL,
+  `lat` float(10,6) NOT NULL,
+  `lon` float(10,6) NOT NULL,
+  `event_time` datetime NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `title` text,
+  `description` text,
+  `posted` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_source_source_id` (`user_id`,`source`,`source_id`),
+  KEY `lon` (`lon`) USING BTREE,
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=471 DEFAULT CHARSET=utf8
